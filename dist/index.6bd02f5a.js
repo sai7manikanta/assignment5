c9102cd879acca4e6cfbd4fbcfc9918906c098ae
sai7manikanta <msaimanikantarao@gmail.com>
2023-04-06 23:15:22 -0700

    commit message

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("h1", {
+    id: "title"
+}, "Heading 1", "Heading 1 for parcel");
+const heading2 = React.createElement("div", {
+    id: "container",
+    hello: "world"
+}, [
+    heading,
+    heading2
+]);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
